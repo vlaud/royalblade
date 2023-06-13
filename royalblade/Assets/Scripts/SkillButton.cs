7a60b20ebaf3d968ecbf51c691b1aff7421c7db0
vlaud <whehdgus1207@naver.com>
2023-06-13 13:03:22 +0900

    아이콘 설치

--- royalblade/Assets/Scripts/SkillButton.cs
+++ royalblade/Assets/Scripts/SkillButton.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
-public class SkillButton : MonoBehaviour
+public class SkillButton : MonoBehaviour, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public UnityEvent myAction;
+    public Image myImage;
+    public float coolTime = 2.0f;
+    Coroutine act = null;
+    
+    public void OnPointerClick(PointerEventData eventData)
     {
-        
+        if (eventData.clickCount == 2)
+        {
+            OnAction();
+        }
+    }
+    public void OnAction()
+    {
+        if (act != null) return;
+        act = StartCoroutine(Cooling());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Cooling()
     {
-        
+        myAction?.Invoke();
+        myImage.fillAmount = 0.0f;
+        float speed = 1.0f / coolTime;
+        while (myImage.fillAmount < 1.0f)
+        {
+            myImage.fillAmount += speed * Time.deltaTime;
+            yield return null;
+        }
+        act = null;
     }
 }
